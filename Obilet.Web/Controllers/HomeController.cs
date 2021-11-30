b23389a19fee672868f3b9eabf512bc2e72036bb
Soner Ardan Şişman <ardann.68@gmail.com>
2021-11-30 15:19:47 +0300

    --Refactoring

--- Obilet.Web/Controllers/HomeController.cs
+++ Obilet.Web/Controllers/HomeController.cs
@@ -39,42 +39,19 @@ namespace Obilet.Web.Controllers
             var ip = HttpContext.Connection.RemoteIpAddress.ToString();
 
             var cacheKeyUser = string.Format(User, ip);
-            var userResult = _redisCacheService.Get<UserModel>(cacheKeyUser);
+            var userCacheResult = _redisCacheService.Get<UserModel>(cacheKeyUser);
 
             GetBusLocationResponseModel busLocationResponse = new();
             #endregion
 
             #region In memory User 
-            if (userResult != null)
-            {
-                #region GetBusLocation Request
-                busLocationResponse = _obiletService.GetBusLocation(new GetBusLocationRequestModel { Language = "tr-TR", DeviceSession = new DeviceSession { DeviceId = userResult.DeviceId, SessionId = userResult.SessionId }, Date = DateTimeOffset.Now });
-
-                if (busLocationResponse.Status.ToLower() == "success")
-                {
-                    var cacheKeyBusLocations = string.Format(BusLocations);
-                    _redisCacheService.Set(cacheKeyBusLocations, busLocationResponse);
-                    return View(new TripModel { BusLocations = busLocationResponse, LastSearchModel = new LastSearchModel { LastOriginId = userResult.LastSearchModel.LastOriginId, LastDestinationId = userResult.LastSearchModel.LastDestinationId, LastDepartureDate = userResult.LastSearchModel.LastDepartureDate } });
-                }
-                else
-                {
-                    TempData["message"] = busLocationResponse.UserMessage;
-                    return View(new TripModel { BusLocations = new GetBusLocationResponseModel(), LastSearchModel = new LastSearchModel() });
-                }
-                #endregion
-            }
-            #endregion
-
-            #region Not in memory User
-            else
+            if (userCacheResult == null)
             {
                 #region GetSession Request
                 var response = _obiletService.GetSession(new GetSessionRequestModel { Connection = new Connection { IpAddress = ip, Port = 5117 }, Browser = new Browser { Name = "Chrome", Version = "47.0.0.12" }, Type = 1 });
 
                 if (response.Status.ToLower() == "success")
                 {
-                    var cacheKeyBusLocations = string.Format(BusLocations);
-                    _redisCacheService.Set(cacheKeyBusLocations, busLocationResponse);
                     var user = new UserModel { Ip = ip, SessionId = response.Data.SessionId, DeviceId = response.Data.DeviceId };
                     _redisCacheService.Set(cacheKeyUser, user);
                 }
@@ -84,23 +61,27 @@ namespace Obilet.Web.Controllers
                     return View(new TripModel { BusLocations = new GetBusLocationResponseModel(), LastSearchModel = new LastSearchModel() });
                 }
                 #endregion
-                #region GetBusLocation Request
-                busLocationResponse = _obiletService.GetBusLocation(new GetBusLocationRequestModel { Language = "tr-TR", DeviceSession = new DeviceSession { DeviceId = response.Data.DeviceId, SessionId = response.Data.SessionId }, Date = DateTimeOffset.Now });
-                if (busLocationResponse.Status.ToLower() == "success")
-                {
-                    return View(new TripModel { BusLocations = busLocationResponse, LastSearchModel = new LastSearchModel() });
-
-                }
-                else
-                {
-                    TempData["message"] = busLocationResponse.UserMessage;
-                    return View(new TripModel { BusLocations = new GetBusLocationResponseModel(), LastSearchModel = new LastSearchModel() });
-
-                }
-                #endregion
             }
             #endregion
 
+            #region GetBusLocation Request
+
+            userCacheResult = _redisCacheService.Get<UserModel>(cacheKeyUser);
+            busLocationResponse = _obiletService.GetBusLocation(new GetBusLocationRequestModel { Language = "tr-TR", DeviceSession = new DeviceSession { DeviceId = userCacheResult.DeviceId, SessionId = userCacheResult.SessionId }, Date = DateTimeOffset.Now });
+
+            if (busLocationResponse.Status.ToLower() == "success")
+            {
+                var cacheKeyBusLocations = string.Format(BusLocations);
+                _redisCacheService.Set(cacheKeyBusLocations, busLocationResponse);
+                return View(new TripModel { BusLocations = busLocationResponse, LastSearchModel = new LastSearchModel { LastOriginId = userCacheResult.LastSearchModel.LastOriginId, LastDestinationId = userCacheResult.LastSearchModel.LastDestinationId, LastDepartureDate = userCacheResult.LastSearchModel.LastDepartureDate } });
+            }
+            else
+            {
+                TempData["message"] = busLocationResponse.UserMessage;
+                return View(new TripModel { BusLocations = new GetBusLocationResponseModel(), LastSearchModel = new LastSearchModel() });
+            }
+            #endregion
+           
         }
 
         [HttpPost]
@@ -130,13 +111,14 @@ namespace Obilet.Web.Controllers
             _redisCacheService.Set(cacheKeyUser, user);
             #endregion
 
-            #region Set BusLocation in Redis Cache
+            #region Get BusLocations in Redis Cache
             var cacheKeyBusLocations = string.Format(BusLocations);
             var busLocations = _redisCacheService.Get<GetBusLocationResponseModel>(cacheKeyBusLocations);
             #endregion
 
             #region GetJourney Request
             var journeysResponse = _obiletService.GetJourney(new GetBusJourneyRequestModel { DeviceSession = new DeviceSession { DeviceId = userResult.DeviceId, SessionId = userResult.SessionId }, Data = new Data { OriginId = data.OriginId, DestinationId = data.DestinationId, DepartureDateStr = data.DepartureDate.DateTime.ToString("yyyy-MM-dd") }, Language = "tr-TR", Date = data.DepartureDate.ToString("yyyy-MM-dd") });
+            
             journeysResponse.Data = journeysResponse.Data.OrderBy(x => x.Journey.Departure).ToArray();
 
             if (journeysResponse.Status.ToLower() == "success")
